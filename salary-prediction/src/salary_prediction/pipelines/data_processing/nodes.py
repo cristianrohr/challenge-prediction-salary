e6b427d10b8496dc05de5b689a9e4c214255237b
cristian <cristianrohr768@gmail.com>
2025-04-12 12:44:14 -0300

    generated X_train and X_test datasets

--- salary-prediction/src/salary_prediction/pipelines/data_processing/nodes.py
+++ salary-prediction/src/salary_prediction/pipelines/data_processing/nodes.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def merge_datasets(salary_df: pd.DataFrame, people_df: pd.DataFrame, descriptions_df: pd.DataFrame):
     """
@@ -31,3 +32,28 @@ def clean_data(data: pd.DataFrame, parameters: dict) -> pd.DataFrame:
     df = df.dropna(subset=[parameters["target_variable"]])
 
     return df
+
+def split_data(data: pd.DataFrame, parameters: dict) -> tuple:
+    """
+    Splits data into features and targets training and test sets.
+
+    Args:
+        data: Data containing features and target.
+        parameters: Parameters defined in parameters/data_processing.yml.
+    Returns:
+        Split data.
+    """
+    
+    # Extract the target variable
+    X = data.drop(parameters["target_variable"], axis=1)
+    y = data[parameters["target_variable"]]
+    
+    # Split the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, 
+        test_size=parameters["test_size"], 
+        random_state=parameters["random_state"]
+    )
+    
+    return X_train, X_test, y_train, y_test
+    
