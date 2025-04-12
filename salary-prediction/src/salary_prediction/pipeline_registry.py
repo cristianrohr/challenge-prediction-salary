2bbc9eed096ab0e6c754c1584e7428f9f9654222
cristian <cristianrohr768@gmail.com>
2025-04-12 12:26:52 -0300

    modified the data_processing kedro template to implement merge and clean methods

--- salary-prediction/src/salary_prediction/pipeline_registry.py
+++ salary-prediction/src/salary_prediction/pipeline_registry.py
@@ -3,6 +3,7 @@
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from salary_prediction.pipelines import data_processing
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,6 +11,10 @@ def register_pipelines() -> dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    
+    data_processing_pipeline = data_processing.create_pipeline()
+
+    return {
+        "data_processing": data_processing_pipeline,
+        "__default__": data_processing_pipeline
+    }
--- salary-prediction/src/salary_prediction/pipelines/data_processing/pipeline.py
+++ salary-prediction/src/salary_prediction/pipelines/data_processing/pipeline.py
@@ -1,28 +1,22 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import clean_data, merge_datasets
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
+                func=merge_datasets,
+                inputs=["salary_raw", "people_raw", "descriptions_raw"],
+                outputs="merged_data",
+                name="merge_datasets_node",
             ),
             node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
-            ),
-            node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
-            ),
+                func=clean_data,
+                inputs="merged_data",
+                outputs="cleaned_data",
+                name="clean_data_node",
+            )
         ]
     )
