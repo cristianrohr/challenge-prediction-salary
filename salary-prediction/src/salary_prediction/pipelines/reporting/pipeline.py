ca8485ac60f6b14d738b69f8ab1bd3c7e65e3dd5
cristian <cristianrohr768@gmail.com>
2025-04-14 19:53:53 -0300

    added functions to choose the best model and secure reproducibility

--- salary-prediction/src/salary_prediction/pipelines/reporting/pipeline.py
+++ salary-prediction/src/salary_prediction/pipelines/reporting/pipeline.py
@@ -1,7 +1,7 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import (
-    plot_metrics
+    plot_metrics, select_best_model, generate_comparison_report, load_all_metrics_from_folder, generate_sorted_metrics_csv
 )
 
 
@@ -15,5 +15,30 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs=None,  # No Kedro dataset output for the plot node
                 name="plot_metrics_node",
             ),
+
+            node(
+                func=load_all_metrics_from_folder,
+                inputs="params:reporting.metrics_folder",
+                outputs="all_model_metrics",
+                name="load_all_metrics_node",
+            ),
+            node(
+                func=select_best_model,
+                inputs=["all_model_metrics", "params:reporting.selection_metric", "params:reporting.selection_ascending"],
+                outputs="best_model_result",
+                name="select_best_model_node",
+            ),
+            node(
+                func=generate_comparison_report,
+                inputs=["all_model_metrics", "params:reporting.full_metrics_report_path"],
+                outputs=None,
+                name="generate_model_comparison_report_node",
+            ),
+            node(
+                func=generate_sorted_metrics_csv,
+                inputs=["all_model_metrics", "params:reporting.selection_metric", "params:reporting.selection_ascending"],
+                outputs="sorted_model_metrics",
+                name="generate_sorted_metrics_csv_node"
+            )
         ]
     )
