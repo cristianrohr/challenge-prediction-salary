cb8d24ddbfc80d8b5bbe75e684fcc3278267e460
cristian <cristianrohr768@gmail.com>
2025-04-14 23:57:09 -0300

    added tags to the data processing pipeline

--- salary-prediction/src/salary_prediction/pipelines/data_processing/pipeline.py
+++ salary-prediction/src/salary_prediction/pipelines/data_processing/pipeline.py
@@ -11,30 +11,35 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["salary_raw", "people_raw", "descriptions_raw"],
                 outputs="merged_data",
                 name="merge_datasets_node",
+                tags=["data_processing", "merge"],
             ),
             node(
                 func=clean_data,
-                inputs=["merged_data",  "params:data_processing"],
+                inputs=["merged_data", "params:data_processing"],
                 outputs="cleaned_data",
                 name="clean_data_node",
+                tags=["data_processing", "clean"],
             ),
             node(
                 func=split_data,
                 inputs=["cleaned_data", "params:data_processing"],
                 outputs=["X_train", "X_test", "y_train", "y_test"],
                 name="split_data_node",
+                tags=["data_processing", "split"],
             ),
             node(
                 func=build_preprocessing_pipeline,
                 inputs=["X_train", "X_test", "params:data_processing"],
                 outputs=["X_train_preprocessed", "X_test_preprocessed", "preprocessor"],
                 name="build_preprocessing_pipeline_node",
+                tags=["data_processing", "preprocessing_v1"],
             ),
             node(
                 func=build_preprocessing_pipeline_v2,
                 inputs=["X_train", "X_test", "params:data_processing"],
                 outputs=["X_train_preprocessed_v2", "X_test_preprocessed_v2", "preprocessor_v2"],
                 name="build_preprocessing_pipeline_v2_node",
+                tags=["data_processing", "preprocessing_v2"],
             ),
         ]
     )
